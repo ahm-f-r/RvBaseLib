f9b451386d15180c67f363aa2b40d26bb2410e3d
Ahmed Rahim <ahmedrahim@Ahmeds-MacBook-Air.local>
2025-07-31 21:53:22 +0100

    [Bring-up] TODO: Finish AddrObject.

--- components/source/include/RvBaseAddrObject.h
+++ components/source/include/RvBaseAddrObject.h
@@ -13,19 +13,29 @@
 #include "RvBaseObject.h"
 
 using namespace std;
+using namespace RvAddrEnums;
 
 class RvBaseAddrObject : public RvBaseObject {
   private:
-    uint64_t mAddr {0};
+    uint64_t                    mAddr;
+    RvAddrEnums::AddrType       mAddrType;
+    RvAddrEnums::PageSize       mPageSize; 
+    RvAddrEnums::CacheLineSize  mCacheLSize;
   
   public:
     // ========================================================================
     explicit RvBaseAddrObject(
-      uint64_t              _addr,
-      string                _name   = "RvBaseAddrObject", 
-      uint64_t              _id     = 0,
-      RvBaseObject const *  _parent = nullptr) : 
-        mAddr (_addr)
+      uint64_t                   _addr,
+      RvAddrEnums::AddrType      _type    = cPhysical,
+      RvAddrEnums::PageSize      _pg_sz   = cSize4k, 
+      RvAddrEnums::CacheLineSize _cl_sz   = cSize64B,
+      string                     _name    = "RvBaseAddrObject", 
+      uint64_t                   _id      = 0,
+      RvBaseObject const *       _parent  = nullptr) :
+        mAddr       (_addr),
+        mAddrType   (_type),
+        mPageSize   (_pg_sz),
+        mCacheLSize (_cl_sz)
     // ========================================================================
     {
       RvBaseObject(_name, _id, _parent);
@@ -39,30 +49,46 @@ class RvBaseAddrObject : public RvBaseObject {
     }
 
     // ========================================================================    
-    template <typename T> uint64_t Addr() const {
+    template <typename T> uint64_t Addr() const
     // ========================================================================
+    {
       return mAddr & RvBaseAddrUtils::Mask<T>();
     }
 
+    // ========================================================================    
+    template <typename T> uint64_t Offset() const
     // ========================================================================
-    void Addr(uint64_t _addr) {
+    {
+      return mAddr & RvBaseAddrUtils::Offset<T>();
+    }
+
+    // ========================================================================    
+    template <typename T> uint64_t Next() const
     // ========================================================================
+    {
+      return mAddr & RvBaseAddrUtils::Offset<T>();
+    }
+
+    // ========================================================================
+    void Addr(uint64_t _addr)
+    // ========================================================================
+    {
       mAddr = _addr;
     }
+    
+    // // ========================================================================
+    // template <typename T> void Property(string _name, T _value)
+    // // ========================================================================
+    // {
+    //   Property<T>(_name, _value);
+    // }
 
-    // ========================================================================
-    template <typename T> void SetProperty(string _name, T _value)
-    // ========================================================================
-    {
-      Set<T>(_name, _value);
-    }
-
-    // ========================================================================
-    template <typename T> optional<T> GetProperty(string _name) const
-    // ========================================================================
-    {
-      return Get<T>(_name);
-    }
+    // // ========================================================================
+    // template <typename T> optional<T> Property(string _name) const
+    // // ========================================================================
+    // {
+    //   return Get<T>(_name);
+    // }
 };
 
 #endif
--- components/source/include/RvBaseEnums.h
+++ components/source/include/RvBaseEnums.h
@@ -1,59 +1,66 @@
 #ifndef RV_BASE_ENUM_H
 #define RV_BASE_ENUM_H
 
-namespace RvAddrEnums {
-  enum eAddrType {
-    SysRegAddr      = 0,
-    DeviceAddr      = 1,
-    MemoryAddr      = 2,
-    PhysicalAddr    = 3,
-    VirtualAddr     = 4,
-    CacheLineAddr   = 5,
-    PageAddr        = 6
+// ============================================================================
+namespace RvAddrEnums 
+// ============================================================================
+{
+  enum AddrType {
+    cPhysical   = 0,
+    cLogical    = 1,
+    cEffective  = 2
   };
-  enum eOffsetType {
-    CacheLineOffset = 0,
-    PageOffset      = 1
+  enum StorageType {
+    cRegister   = 0,
+    cMemory     = 1, 
+    cDevice     = 2
   };
-  enum eWordSize {
-    Byte  = 0, 
-    HWord = 1,
-    Word  = 2,
-    DWord = 3,
-    QWord = 4
+  enum OffsetType {
+    cCacheLine  = 0,
+    cPage       = 1
   };
-  enum eCacheLineSize {
-    Size32B   = 5,
-    Size64B   = 6,
-    Size128B  = 7,
-    Size256B  = 8,
-    Size512B  = 9
+  enum WordSize {
+    cByte       = 0, 
+    cHWord      = 1,
+    cWord       = 2,
+    cDWord      = 3,
+    cQWord      = 4
   };
-  enum ePageSize {
-    Size1k  = 10,
-    Size2k  = 11,
-    Size4k  = 12,
-    Size8k  = 13,
-    Size16k = 14,
-    Size1M  = 20,
-    Size2M  = 21,
-    Size4M  = 22,
-    Size8M  = 23,
-    Size16M = 24,
-    Size1G  = 30
+  enum CacheLineSize {
+    cSize32B    = 5,
+    cSize64B    = 6,
+    cSize128B   = 7,
+    cSize256B   = 8,
+    cSize512B   = 9
+  };
+  enum PageSize {
+    cSize1k     = 10,
+    cSize2k     = 11,
+    cSize4k     = 12,
+    cSize8k     = 13,
+    cSize16k    = 14,
+    cSize1M     = 20,
+    cSize2M     = 21,
+    cSize4M     = 22,
+    cSize8M     = 23,
+    cSize16M    = 24,
+    cSize1G     = 30
   };
 }
 
-namespace RvDataEnums {
-  enum eDataMask {
-    DwordHi = 0xffffffff00000000,
-    DwordLo = 0x00000000ffffffff,
-    WordHi  = 0x00000000ffff0000, 
-    WordLo  = 0x000000000000ffff,
-    HwordHi = 0x000000000000ff00,
-    HwordLo = 0x00000000000000ff
-    ByteHi  = 0x00000000000000f0
-    ByteLo  = 0x000000000000000f
+// ============================================================================
+namespace RvDataEnums
+// ============================================================================
+{
+  enum DataMask {
+    cDwordHi    = 0xffffffff00000000,
+    cDwordLo    = 0x00000000ffffffff,
+    cWordHi     = 0x00000000ffff0000, 
+    cWordLo     = 0x000000000000ffff,
+    cHwordHi    = 0x000000000000ff00,
+    cHwordLo    = 0x00000000000000ff,
+    cByteHi     = 0x00000000000000f0,
+    cByteLo     = 0x000000000000000f
   };
 }
 
